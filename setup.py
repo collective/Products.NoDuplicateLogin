e99b7f4e1ec68ea15ca49035598c84d4c4645dc9
Alex Clark <aclark@aclark.net>
2010-12-01 01:27:41 +0000

    Put things in the right place ; flesh out setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    version='1.0 svn/dev',
     name='Products.NoDuplicateLogin',
     author='Jens Klein',
     author_email='jens@bluedynamics.com',
+    namespace_packages=['Products'],
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        ]
     )
