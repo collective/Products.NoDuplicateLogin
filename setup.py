d4be613302c9365e9c63f6060b2b2d2a6bfe617b
Alex Clark <aclark@aclark.net>
2010-12-18 02:27:18 +0000

    Fix up the docs

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 
 from setuptools import setup, find_packages
 
+def read(filename):
+    return open(filename, 'rb').read()
+
 setup(
     version='1.0a1',
     name='Products.NoDuplicateLogin',
+    description='Products.NoDuplicateLogin',
+    long_description=read('README.txt') + read('docs/HISTORY.txt'),
     author='Daniel Nouri',
     author_email='daniel.nouri@gmail.com',
     namespace_packages=['Products'],
